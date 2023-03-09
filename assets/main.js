d2345a3a8ec186f33a775ab36c2d88cf2f742c8d
marcelin <marcelintingougoui3@gmail>
2023-03-09 13:18:42 +0100

    declaration des varriable et ceation des fonction ferme

--- assets/main.js
+++ assets/main.js
@@ -4,9 +4,17 @@
 **/
 
 // Commence par déclarer les variabls necessaires
+const saidebar_menu=document.querySelector(".sidebar_menu")
+const saidebar = document.querySelector(".sidebar")
 
 
 
 // Rédige les conditions ou fonctions juste ici, n'oublie pas le résultat attendu dans le README.md
 
+saidebar_menu.addEventListener("click" ,ferme)
 
+function ferme(){
+    saidebar.classList.toggle("sidebarferme")
+
+
+}
